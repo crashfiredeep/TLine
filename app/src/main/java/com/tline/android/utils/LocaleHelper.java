b9bbcceed0de5682730156baaf2b4a0aa7799340
Naeem <naeemark@gmail.com>
2017-11-28 12:45:21 +0400

    + adds code to relaunch Activity

--- app/src/main/java/com/tline/android/utils/LocaleHelper.java
+++ app/src/main/java/com/tline/android/utils/LocaleHelper.java
@@ -1,9 +1,12 @@
 package com.tline.android.utils;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 
+import com.tline.android.features.splash.SplashActivity;
 import com.tline.android.features.timeline.view.impl.TimelineActivity;
 
 import java.util.Locale;
@@ -24,15 +27,22 @@ public class LocaleHelper {
         config.locale = newLocale;
         config.setLayoutDirection(newLocale);
         resources.updateConfiguration(config, resources.getDisplayMetrics());
-        activity.recreate();
+        relaunch(activity.getApplicationContext());
     }
 
     private static Locale getNewLocale(Configuration configuration) {
         if (configuration.locale.getLanguage().equals("ar")){
             return new Locale("en");
         }else {
-
             return new Locale("ar");
         }
+    }
+
+    private static void relaunch(Context context) {
+        Intent i = new Intent(context, TimelineActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
     }
 }
