8f0635cc3977eb081d4a255ff518ca77ce8aea28
Naeem <naeemark@gmail.com>
2017-11-30 02:34:47 +0400

    + adds tests for login

--- app/src/main/java/com/tline/android/features/login/presenter/impl/LoginPresenterImpl.java
+++ app/src/main/java/com/tline/android/features/login/presenter/impl/LoginPresenterImpl.java
@@ -6,6 +6,7 @@ import com.tline.android.app.presenter.impl.BasePresenterImpl;
 import com.tline.android.features.login.presenter.LoginPresenter;
 import com.tline.android.features.login.view.LoginView;
 import com.tline.android.features.login.interactor.LoginInteractor;
+import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 
@@ -28,9 +29,20 @@ public final class LoginPresenterImpl extends BasePresenterImpl<LoginView> imple
     public void onStart(boolean viewCreated) {
         super.onStart(viewCreated);
 
-        if (viewCreated){
-            assert mView != null;
-            mView.updateUi();
+        if (viewCreated) {
+            checkSession(TwitterCore.getInstance().getSessionManager().getActiveSession());
+        }
+    }
+
+    @Override
+    public void checkSession(TwitterSession activeSession) {
+
+        assert mView != null;
+        if (activeSession == null) {
+            mView.showLoginUi();
+        } else {
+            mView.updateUi(activeSession.getUserName());
+            mView.launchHomeActivity();
         }
     }
 
@@ -42,20 +54,18 @@ public final class LoginPresenterImpl extends BasePresenterImpl<LoginView> imple
 
     @Override
     public void handleLoginSuccess(TwitterSession twitterSession) {
-        assert mView != null;
-        mView.launchHomeActivity();
+        // to persist session data
     }
 
     @Override
     public void handleLoginFailure(TwitterException twitterException) {
-        assert mView != null;
-        mView.showLoginError(twitterException.getMessage());
+        // to work with Exception
     }
 
     @Override
     public void logout() {
         assert mView != null;
         mView.logoutTwitter();
-        mView.updateUi();
+        mView.showLoginUi();
     }
 }
