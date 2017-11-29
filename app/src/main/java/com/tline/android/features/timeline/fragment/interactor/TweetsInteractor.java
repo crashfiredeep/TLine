dac405f28efb4882202d9ad9e802bbb18492044a
Naeem <naeemark@gmail.com>
2017-11-30 02:57:40 +0400

    + adds tests for TweetsPresenter

--- app/src/main/java/com/tline/android/features/timeline/fragment/interactor/TweetsInteractor.java
+++ app/src/main/java/com/tline/android/features/timeline/fragment/interactor/TweetsInteractor.java
@@ -3,12 +3,27 @@ package com.tline.android.features.timeline.fragment.interactor;
 import com.tline.android.app.interactor.BaseInteractor;
 import com.tline.android.features.timeline.fragment.presenter.TweetsPresenter;
 import com.tline.android.features.timeline.fragment.presenter.impl.TweetsPresenterImpl;
+import com.twitter.sdk.android.core.models.Tweet;
+
+import java.util.List;
 
 public interface TweetsInteractor extends BaseInteractor {
 
     boolean isNetworkConnected();
 
-    void fetchTweets(String twitterHandle, TweetsPresenter.OnFetchDataListener listener);
+    void fetchTweets(String twitterHandle, OnFetchDataListener listener);
 
     String getErrorString();
+
+
+    interface OnFetchDataListener {
+
+        void onStart();
+
+        void onSuccess(List<Tweet> tweets);
+
+        void onFailure(String message);
+
+        void onComplete();
+    }
 }
